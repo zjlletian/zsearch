b1ff1549420ec28321cea0a4a6545570dc389ba8
Zhou Junlong <zjl19941026@live.com>
2016-02-17 23:35:55 +0800

    build program

---
+++ Config.php
@@ -0,0 +1,14 @@
+<?php
+define('APPROOT',dirname(__FILE__));
+
+//设置include包含文件所在的所有目录
+$include_path=get_include_path();  
+$include_path.=PATH_SEPARATOR.APPROOT."/lib"; 
+$include_path.=PATH_SEPARATOR.APPROOT."/class";
+set_include_path($include_path);
+
+//默认时区
+date_default_timezone_set('Asia/Shanghai');
+
+//Zspider API 地址
+$GLOBALS['ZSPIDERAPI'] = 'http://spider.zhouvm.com/';
